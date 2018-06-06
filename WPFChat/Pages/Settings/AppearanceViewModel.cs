4690f3eaa4af87da76e69d2ef5c0812751e56180
Drakopit <patrickcamelo@yahoo.com.br>
2018-06-06 07:32:49 -0300

    Some little changes

--- WPFChat/Pages/Settings/AppearanceViewModel.cs
+++ WPFChat/Pages/Settings/AppearanceViewModel.cs
@@ -15,8 +15,8 @@ namespace WPFChat.Pages.Settings
     public class AppearanceViewModel
         : NotifyPropertyChanged
     {
-        private const string FontSmall = "small";
-        private const string FontLarge = "large";
+        private const string FontSmall = "Pequena";
+        private const string FontLarge = "Grande";
 
         // 9 accent colors from metro design principles
         /*private Color[] accentColors = new Color[]{
@@ -63,8 +63,8 @@ namespace WPFChat.Pages.Settings
         public AppearanceViewModel()
         {
             // add the default themes
-            this.themes.Add(new Link { DisplayName = "dark", Source = AppearanceManager.DarkThemeSource });
-            this.themes.Add(new Link { DisplayName = "light", Source = AppearanceManager.LightThemeSource });
+            this.themes.Add(new Link { DisplayName = "Escuro", Source = AppearanceManager.DarkThemeSource });
+            this.themes.Add(new Link { DisplayName = "Claro", Source = AppearanceManager.LightThemeSource });
 
             this.SelectedFontSize = AppearanceManager.Current.FontSize == FontSize.Large ? FontLarge : FontSmall;
             SyncThemeAndColor();
